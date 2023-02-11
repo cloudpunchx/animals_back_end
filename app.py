42e5e60a3edc401fa68978bdcfe4bae75041f989
cloudpunchx <siobhanbonardi@hotmail.com>
2023-02-11 14:46:16 -0700

    Backed up database

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
-from flask import Flask, request
-import json
+# NOW WE WANT TO: CLICK CHECK STRUCTURE ONLY WHEN DUMPING DATABASE
+
+from flask import Flask, make_response, jsonify
 from dbhelpers import run_statement
+from dbcreds import production_mode
 
 app = Flask(__name__)
 
-app.run(debug = True)
+@app.get('/api/animals')
+def get_animals():
+    result = run_statement("CALL get_all_animals()")
+    keys = ["animalName", "numberOfLegs"]
+    response = []
+    if (type(result) == list):
+        for animal in result:
+            response.append(dict(zip(keys, animal)))
+        return make_response(jsonify(response), 200)
+    else:
+        return make_response(jsonify(response), 500)
+
+if (production_mode == True):
+    print("Running server in Production Mode")
+    import bjoern #type:ignore
+    bjoern.run(app, "0.0.0.0", 5000)
+else:
+    print("Running Server in Testing Mode.")
+    from flask_cors import CORS
+    CORS(app)
+    app.run(debug=True)
