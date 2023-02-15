3cc6418d853c897a34bcfb0befd011f21e24ea51
cloudpunchx <siobhanbonardi@hotmail.com>
2023-02-14 18:34:56 -0700

    PUshing

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # NOW WE WANT TO: CLICK CHECK STRUCTURE ONLY WHEN DUMPING DATABASE
 
+# NOTES: MAKE A SUMMARY SHEET OF THE WEEK 22 SLIDE A FOR CHECKLIST ON FRONT END/BACK END STEPS TO DEPLOY
+
 from flask import Flask, make_response, jsonify
 from dbhelpers import run_statement
 from dbcreds import production_mode
